5611592675185e4e514b82bdef07a6aff40214ac
hgb <3299848100@qq.com>
2022-08-07 16:09:27 +0800

    配置前端路由

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,24 @@
 import React, { lazy, Suspense } from "react"
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '@/pages/Home'
 // const Home = lazy(() => import ('@/pages/Home'))
-
-export default () => {
+const Koalabean = lazy(() => import('@/pages/Kaolabean'))
+const Sort = lazy(() => import('@/pages/Sort'))
+const ShoppingCart = lazy(() => import('@/pages/ShoppingCart'))
+const Mine = lazy(() => import('@/pages/Mine'))
+// const Sort = lazy(() => import('@/pages/Sort'))
+const RouterConfig = () => {
     return (
-        <>
+        <Suspense fallback={null}>
             <Routes>
                 <Route path="/" element={<Home />}></Route>
-                {/* <Route path="/search" element={<Search />}></Route> */}
+                <Route path="/kaolabean" element={<Koalabean />}></Route>
+                <Route path="/sort" element={<Sort />}></Route>
+                <Route path="/shoppingcart" element={<ShoppingCart />}></Route>
+                <Route path="/mine" element={<Mine />}></Route>
             </Routes>
-        </>
+        </Suspense>
     )
 }
 
-
+export default RouterConfig
