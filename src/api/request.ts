4535a43355d965e28616e169c1cb5097f9173608
hgb <3299848100@qq.com>
2022-08-09 01:46:18 +0800

    sidebar 分类数据

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,7 +1,10 @@
 import { axiosInstance } from "./config";
 import {
-    URL_GOODS
+    URL_GOODS,
+    URL_NAVBARS
 } from './url'
 
 export const getGoodsRequest =
     () => axiosInstance.get(URL_GOODS)
+export const getNavbarsRequest =
+    () => axiosInstance.get(URL_NAVBARS)
--- src/api/url.ts
+++ src/api/url.ts
@@ -1,5 +1,7 @@
 const URL_GOODS = '/goods'
+const URL_NAVBARS = './navbars'
 
 export {
-    URL_GOODS
+    URL_GOODS,
+    URL_NAVBARS
 }
