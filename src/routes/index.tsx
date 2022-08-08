117330583a74c3656d6f3851e8f2b3ae3b4303a0
hgb <3299848100@qq.com>
2022-08-09 01:43:29 +0800

    detail Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,16 +6,20 @@ const Koalabean = lazy(() => import('@/pages/Kaolabean'))
 const Sort = lazy(() => import('@/pages/Sort'))
 const ShoppingCart = lazy(() => import('@/pages/ShoppingCart'))
 const Mine = lazy(() => import('@/pages/Mine'))
-// const Sort = lazy(() => import('@/pages/Sort'))
+const Search = lazy(() => import('@/pages/Search'))
+const Detail = lazy(() => import('@/pages/Detail'))
 const RouterConfig = () => {
     return (
         <Suspense fallback={null}>
             <Routes>
-                <Route path="/" element={<Home />}></Route>
+                <Route path="/" element={<Navigate to="/home" replace={true} />}></Route>
+                <Route path="/home" element={<Home />}></Route>
                 <Route path="/kaolabean" element={<Koalabean />}></Route>
                 <Route path="/sort" element={<Sort />}></Route>
                 <Route path="/shoppingcart" element={<ShoppingCart />}></Route>
                 <Route path="/mine" element={<Mine />}></Route>
+                <Route path="/search" element={<Search />}></Route>
+                <Route path="/detail/:id" element={<Detail />} />
             </Routes>
         </Suspense>
     )
